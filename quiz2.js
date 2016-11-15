d606f570932313e4dcbddcefa9209ca5aeae1e20
ErikaReichert <emr74@pitt.edu>
2016-11-14 19:37:53 -0500

    Quiz 2
    Here is my quiz!

--- quiz2.js
+++ quiz2.js
@@ -1,3 +1,6 @@
-consle.log('welocme to quiz town, population...YOU!')
+(function () {
 
-@('.dynamic').hmtl('<div p>HI THERE, I\'M FROM JAVASCRIPT</p></div>').fadeIn('slo')
+    console.log('Welcome to quiz town, population...YOU!');
+
+    $('.dynamic').hmtl('<div><p>HI THERE, I\'M FROM JAVASCRIPT</p></div>').fadeIn('slow');
+})();
